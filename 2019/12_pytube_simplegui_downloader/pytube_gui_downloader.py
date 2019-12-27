e72a8a1b4b9fd06f3c8209557395351cdd0629a5
daewonyoon <daewon.yoon@gmail.com>
2019-12-27 18:45:15 +0900

    여러 화질 중에서 고르기.

--- 2019/12_pytube_simplegui_downloader/pytube_gui_downloader.py
+++ 2019/12_pytube_simplegui_downloader/pytube_gui_downloader.py
@@ -8,9 +8,18 @@ sg.change_look_and_feel('Material1')
 layout = [[sg.Text('copy youtube url below :'), sg.Text(size=(25,1), key='-OUTPUT-')],
           [sg.Text('youtube url', size=(10, 1)),sg.Input(key='-URL-')],
           [sg.Text('save folder', size=(10, 1)),sg.Input(key='-DIR-'), sg.FolderBrowse()],
+#          [sg.Combo(('',), key='combo1')],
           [sg.Button('Save'), sg.Button('Exit')]]
 
 window = sg.Window('Youtube Downloader', layout)
+
+
+#def PopupSelect(lst, title='Select Popup'):
+#    sel_idx = 0
+#    layout = [[ListBox(values=lst)],
+#              [Button('Ok', bind_return_key=True)]]
+#
+#    return sel_idx
 
 
 def save_youtube(url, folder):
@@ -22,14 +31,21 @@ def save_youtube(url, folder):
     yt = pytube.YouTube(url)
 
     vids = yt.streams.all()
-    for i, v in enumerate(vids):
-        print('%3d : %s'%(i, v))
 
-    vids[0].download(folder)
-    filename = vids[0].default_filename
+    l = []
+    for i, v in enumerate(vids):
+        l.append('%3d : %s'%(i, v))
+        print(l[-1])
+
+    text = sg.PopupGetText('\n'.join(l), title='Select Video')    
+    #window['combo1'].Update(values=l)
+    i = int(text)
+
+    vids[i].download(folder)
+    filename = vids[i].default_filename
 
     if os.path.exists(os.path.join(folder, filename)):
-        return True, vids[0].default_filename
+        return True, vids[i].default_filename
     return False, "unknown error"
 
 
@@ -49,5 +65,6 @@ while True:  # Event Loop
         else:
             result = 'Failed. ' + msg
         window['-OUTPUT-'].update(result)
+
 
 window.close()
