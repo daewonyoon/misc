e93395fa6a3ef3e605071ebf824e37c35698b1cd
daewonyoon <daewon.yoon@gmail.com>
2021-02-27 03:51:33 +0900

    tk style

--- 2021/02_random_bible/rand_bible_gui_tk.py
+++ 2021/02_random_bible/rand_bible_gui_tk.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import ttk
+from tkinter.ttk import *
 
 import random
 
@@ -8,7 +10,6 @@ class Bible:
         self.bible_list = self.get_bible()
 
     def get_bible(self):
-
         file_name = "개역한글판성경.txt"
         with open(file_name, "r", encoding="cp949") as f:
             bible_list = [line for line in f.readlines()]
@@ -20,17 +21,26 @@ class Bible:
 
 def main():
     win = Tk()
+    ttk.Style().theme_use("clam")
     bible = Bible()
 
     win.title("Today's Word")
-    lbl = Label(win, text="버튼을 눌러, 오늘의 말씀을 뽑으세요.")
-    lbl.grid(column=0, row=1)
+    lbl = Label(win, text="버튼을 눌러, 오늘의 말씀을 뽑으세요.", wraplength=600, justify="left", relief="sunken")
+    lbl.grid(column=0, row=0, ipadx=10, ipady=10, padx=10, pady=10)
 
     def clicked():
         lbl.configure(text=bible.choose_one())
 
     btn = Button(win, text="말씀뽑기", command=clicked)
-    btn.grid(column=1, row=2)
+    btn.grid(
+        column=0,
+        row=1,
+        sticky="sw",
+        ipadx=10,
+        ipady=10,
+        padx=10,
+        pady=10,
+    )
 
     win.mainloop()
 
