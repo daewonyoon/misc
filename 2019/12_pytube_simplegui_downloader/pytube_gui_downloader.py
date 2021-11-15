98ec492e2d6abfeb4bd3358f62e91539aba7b7d7
Daewon YOON <daewon.yoon@gmail.com>
2021-11-15 17:10:56 +0900

    - black - deprecation warning - todo : progress callback

--- 2019/12_pytube_simplegui_downloader/pytube_gui_downloader.py
+++ 2019/12_pytube_simplegui_downloader/pytube_gui_downloader.py
@@ -3,18 +3,20 @@ import PySimpleGUI as sg
 import pytube
 
 
-sg.change_look_and_feel('Material1')
+sg.change_look_and_feel("Material1")
 
-layout = [[sg.Text('copy youtube url below :'), sg.Text(size=(25,1), key='-OUTPUT-')],
-          [sg.Text('youtube url', size=(10, 1)),sg.Input(key='-URL-')],
-          [sg.Text('save folder', size=(10, 1)),sg.Input(key='-DIR-'), sg.FolderBrowse()],
-#          [sg.Combo(('',), key='combo1')],
-          [sg.Button('Save'), sg.Button('Exit')]]
+layout = [
+    [sg.Text("copy youtube url below :"), sg.Text(size=(25, 1), key="-OUTPUT-")],
+    [sg.Text("youtube url", size=(10, 1)), sg.Input(key="-URL-")],
+    [sg.Text("save folder", size=(10, 1)), sg.Input(key="-DIR-"), sg.FolderBrowse()],
+    #          [sg.Combo(('',), key='combo1')],
+    [sg.Button("Save"), sg.Button("Exit")],
+]
 
-window = sg.Window('Youtube Downloader', layout)
+window = sg.Window("Youtube Downloader", layout)
 
 
-#def PopupSelect(lst, title='Select Popup'):
+# def PopupSelect(lst, title='Select Popup'):
 #    sel_idx = 0
 #    layout = [[ListBox(values=lst)],
 #              [Button('Ok', bind_return_key=True)]]
@@ -23,22 +25,22 @@ window = sg.Window('Youtube Downloader', layout)
 
 
 def save_youtube(url, folder):
-    print('url:', url)
-    print('folder:', folder)
+    print("url:", url)
+    print("folder:", folder)
     if len(folder) == 0:
         folder = os.getcwd()
 
     yt = pytube.YouTube(url)
 
-    vids = yt.streams.all()
+    vids = yt.streams  # .all()
 
     l = []
     for i, v in enumerate(vids):
-        l.append('%3d : %s'%(i, v))
+        l.append("%3d : %s" % (i, v))
         print(l[-1])
 
-    text = sg.PopupGetText('\n'.join(l), title='Select Video')    
-    #window['combo1'].Update(values=l)
+    text = sg.PopupGetText("\n".join(l), title="Select Video")
+    # window['combo1'].Update(values=l)
     i = int(text)
 
     vids[i].download(folder)
@@ -52,19 +54,19 @@ def save_youtube(url, folder):
 while True:  # Event Loop
     event, values = window.read()
     print(event, values)
-    if event in  (None, 'Exit'):
+    if event in (None, "Exit"):
         break
-    if event == 'Save':
+    if event == "Save":
         # Update the "output" text element to be the value of "input" element
-        url = values['-URL-']
-        folder = values['-DIR-']
-        window['-OUTPUT-'].update('saving...')
+        url = values["-URL-"]
+        folder = values["-DIR-"]
+        window["-OUTPUT-"].update("saving...")
         result, msg = save_youtube(url, folder)
         if result:
-            result = 'File Saved! ' + msg
+            result = "File Saved! " + msg
         else:
-            result = 'Failed. ' + msg
-        window['-OUTPUT-'].update(result)
+            result = "Failed. " + msg
+        window["-OUTPUT-"].update(result)
 
 
 window.close()
