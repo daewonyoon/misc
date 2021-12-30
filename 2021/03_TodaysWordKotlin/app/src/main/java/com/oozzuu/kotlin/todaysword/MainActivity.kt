70bb7dcfff841d2a84563ce6b631e1a6e10084d2
Daewon YOON <daewon.yoon@gmail.com>
2021-12-30 16:46:47 +0900

    db init trial...

--- 2021/03_TodaysWordKotlin/app/src/main/java/com/oozzuu/kotlin/todaysword/MainActivity.kt
+++ 2021/03_TodaysWordKotlin/app/src/main/java/com/oozzuu/kotlin/todaysword/MainActivity.kt
@@ -21,15 +21,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         val bible: List<String> =
             applicationContext.assets
                 .open("개역한글판성경.utf8.txt")
                 .bufferedReader()
                 .use { it.readText() }
                 .lines()
-
+                
         binding.button.setOnClickListener {
             binding.textView.text = bible.random()
         }
+         /*
+        val bible: List<Bible> = db.bibleDao().getAll()
+
+        binding.button.setOnClickListener {
+            binding.textView.text = bible.random().text
+        }
+         */
+
     }
 }
