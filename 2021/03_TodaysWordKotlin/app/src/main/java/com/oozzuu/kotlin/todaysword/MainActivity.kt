3ae5365328fcb09062f1435e0d6c366d696c5e7f
Daewon YOON <daewon.yoon@gmail.com>
2021-12-31 10:03:17 +0900

    regex line parse

--- 2021/03_TodaysWordKotlin/app/src/main/java/com/oozzuu/kotlin/todaysword/MainActivity.kt
+++ 2021/03_TodaysWordKotlin/app/src/main/java/com/oozzuu/kotlin/todaysword/MainActivity.kt
@@ -28,17 +28,35 @@ class MainActivity : AppCompatActivity() {
                 .bufferedReader()
                 .use { it.readText() }
                 .lines()
-                
-        binding.button.setOnClickListener {
-            binding.textView.text = bible.random()
-        }
-         /*
-        val bible: List<Bible> = db.bibleDao().getAll()
+
 
         binding.button.setOnClickListener {
-            binding.textView.text = bible.random().text
-        }
-         */
+            val parsed = parseLine(bible.random())
+            val book = parsed[0]
+            val ch = parsed[1]
+            val ver = parsed[2]
+            val text = parsed[3]
 
+            binding.tvBookchver.text = "${book} ${ch}장 ${ver}절"
+            binding.textView.text = text
+        }
+        /*
+       val bible: List<Bible> = db.bibleDao().getAll()
+
+       binding.button.setOnClickListener {
+           binding.textView.text = bible.random().text
+       }
+        */
+
+    }
+
+    private fun parseLine(line: String): List<String> {
+
+        val regex = """([^\d]+)(\d+):(\d+) (.*)""".toRegex()
+        val result = regex.find(line)
+        val (book, ch, ver, text) = result!!.destructured
+
+        // TODO: book to full name
+        return listOf(book, ch, ver, text)
     }
 }
