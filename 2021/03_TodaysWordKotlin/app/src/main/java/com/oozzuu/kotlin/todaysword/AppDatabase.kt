5e0c177e9c1ac2251a6320471d41ebdbbe080b1f
Daewon YOON <daewon.yoon@gmail.com>
2022-07-19 13:53:17 +0900

    moved projects to learning_android

--- 2021/03_TodaysWordKotlin/app/src/main/java/com/oozzuu/kotlin/todaysword/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.oozzuu.kotlin.todaysword
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Bible::class], version = 1, exportSchema = false)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun bibleDao(): BibleDao
-}
